d83b8cbb05cc75bb769bbfcd3c9f938876e33473
DESKTOP-27DRGO2\Dominik <s.domnik1321@gmail.com>
2021-11-20 17:19:12 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,13 @@
-import React,{useState,useEffect} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {useSelector,useDispatch} from 'react-redux'
-import { bindActionCreators } from 'redux';
-import { State } from './APIController/reducers'
-import * as MoviesActions from './APIController/action-creators/moviesActions'
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
 
-  const { actors, movies } = useSelector((state:State) => state)
-  const dispatch = useDispatch()
-  const MoviesAPI = bindActionCreators(MoviesActions,dispatch)
-
-  useEffect(()=>{
-
-  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Home} /> 
+      </Switch>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './APIController/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
