a5f07771814b0f13090498af0eca026520b7e73d
insane <insane@00-25-D3-42-8C-28.lazy.com>
2021-11-23 13:06:14 -0500

    fix

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -3,7 +3,6 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 
 const Layout:React.FC<any> = ({children}) => {
-    // test
     return (
         <div className="container">
             <Navbar />
