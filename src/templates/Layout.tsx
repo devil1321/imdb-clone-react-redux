9de9cc6a4f3ae5624f4f4a62b3ced5fa6db5176b
insane <insane@00-25-D3-42-8C-28.lazy.com>
2021-11-24 05:22:21 -0500

    update

--- src/templates/Layout.tsx
+++ src/templates/Layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-
+import '../styles/theme/theme.scss'
 const Layout:React.FC<any> = ({children}) => {
     return (
         <div className="container">
